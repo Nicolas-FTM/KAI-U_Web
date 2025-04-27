ffff609c62f394fb3fcc5d57edcade19a4c57152
Nicolas-FTM <nicolasfelipetm@gmail.com>
2025-04-27 14:03:11 +0200

    test: test upload to master and see it in GitHub Pages

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+        // Tu código JavaScript aquí
+        document.addEventListener('DOMContentLoaded', function() {
+            console.log('El sitio web ha cargado completamente');
+        });
